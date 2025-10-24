7f5523467ea59a7d2dcf81187718241d22acb861
Cbushcun <bushc325@gmail.com>
2025-10-24 18:49:27 -0400

    mongoose prep

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
 import { LoginForm } from "@/components/login-form";
 
 export default function Page() {
-	return <></>;
+	return (
+		<p>
+			Homepage <a href="/login">Login</a> <a href="/register">Register</a>
+		</p>
+	);
 }
---
+++ src/app/profile/page.js
@@ -0,0 +1,3 @@
+export default function Page() {
+	return <h1>Profile Page, you are logged in</h1>;
+}
