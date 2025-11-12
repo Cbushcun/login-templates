f10a101c7f82a6407c39855877cefe79d2ae3cb0
Cbushcun <bushc325@gmail.com>
2025-11-12 18:27:15 -0500

    Altered form elements & user update api endpoint prep

--- src/app/profile/ProfilePage.jsx
+++ src/app/profile/ProfilePage.jsx
@@ -2,11 +2,17 @@
 import { Avatar } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useState } from "react";
+
+/**
+ *
+ * TODO: Fix the form submission and data handling.
+ */
 
 export default function ProfilePage({ user }) {
 	const [canEdit, setCanEdit] = useState(false);
@@ -31,11 +37,7 @@ export default function ProfilePage({ user }) {
 				</CardHeader>
 				<CardContent>
 					{canEdit ? (
-						<Button
-							onClick={() => {
-								toggleEdit();
-							}}
-						>
+						<Button type="submit" form="profile-form">
 							Save
 						</Button>
 					) : (
@@ -54,11 +56,8 @@ export default function ProfilePage({ user }) {
 			{/* Settings Tabs */}
 			<Tabs defaultValue="profile" className="mt-6">
 				<TabsList>
-					<TabsTrigger value="profile">Profile</TabsTrigger>
-					<TabsTrigger value="account">Account</TabsTrigger>
-					<TabsTrigger value="appearance">Appearance</TabsTrigger>
+					<TabsTrigger value="profile">Settings</TabsTrigger>
 				</TabsList>
-
 				{/* Profile Tab */}
 				<TabsContent value="profile">
 					<Card className="mt-4">
@@ -66,144 +65,98 @@ export default function ProfilePage({ user }) {
 							<h3 className="text-lg font-semibold">Profile Information</h3>
 						</CardHeader>
 						<CardContent>
-							<div className="space-y-4">
-								{canEdit ? (
-									<>
-										<div>
-											<Label className={"text-gray-500 mb-1"} htmlFor="name">
-												Name
-											</Label>
-											<Input id="name" defaultValue={user.name} />
-										</div>
-										<div>
-											<Label className={"text-gray-500 mb-1"} htmlFor="title">
-												Title
-											</Label>
-											<Input id="title" defaultValue={user.title}></Input>
-										</div>
-									</>
-								) : (
-									""
-								)}
-								<div>
-									<Label className={"text-gray-500 mb-1"} htmlFor="username">
-										Username
-									</Label>
-									{canEdit ? (
-										<Input id="username" defaultValue={user.username} />
-									) : (
-										<p>{user.username} </p>
-									)}
-								</div>
-								<div>
-									<Label className={"text-gray-500 mb-1"} htmlFor="email">
-										Email
-									</Label>
-									{canEdit ? (
-										<p>
-											{user.email}{" "}
-											<i className="text-gray-500">unavailable at this time</i>
-										</p>
-									) : (
-										<p>{user.email}</p>
-									)}
-								</div>
-								<div>
-									<Label className={"text-gray-500 mb-1"} htmlFor="bio">
-										Bio
-									</Label>
-									{canEdit ? (
-										<Input id="bio" defaultValue={user.bio} />
-									) : (
-										<p>{user.bio}</p>
-									)}
-								</div>
-								<div>
+							<form
+								id="profile-form"
+								action="/api/user/"
+								method="POST"
+								className="space-y-4"
+							>
+								<FieldGroup>
 									{canEdit ? (
 										<>
-											<Label className={"text-gray-500 mb-1"}>
-												Current Password
-											</Label>
-											<Input
-												id="current-password"
-												type="password"
-												placeholder="••••••••"
-											></Input>
-											<Label className={"text-gray-500 mb-1"}>
-												New Password
-											</Label>
-											<Input
-												id="new-password"
-												type="password"
-												placeholder="••••••••"
-											></Input>
-											<Label className={"text-gray-500 mb-1"}>
-												Confirm New Password
-											</Label>
-											<Input
-												id="new-password-confirm"
-												type="password"
-												placeholder="••••••••"
-											></Input>
+											<Field>
+												<FieldLabel htmlFor="name">Name</FieldLabel>
+												<Input id="name" defaultValue={user.name} />
+											</Field>
+											<Field>
+												<FieldLabel
+													className={"text-gray-500 mb-1"}
+													htmlFor="title"
+												>
+													Title
+												</FieldLabel>
+												<Input id="title" defaultValue={user.title}></Input>
+											</Field>
 										</>
 									) : (
-										<>
-											<Label className={"text-gray-500 mb-1"}>Password</Label>
-											<p>••••••••</p>
-										</>
+										""
 									)}
-								</div>
-							</div>
-						</CardContent>
-					</Card>
-				</TabsContent>
-				{/* Account Tab */}
-				<TabsContent value="account">
-					<Card className="mt-4">
-						<CardHeader>
-							<h3 className="text-lg font-semibold">Account Settings</h3>
-						</CardHeader>
-						<CardContent>
-							<div className="space-y-4">
-								<div>
-									<Label htmlFor="password">Password</Label>
-									<Input id="password" type="password" placeholder="••••••••" />
-								</div>
-								<div>
-									<Label htmlFor="twoFactor">Two-Factor Authentication</Label>
-									<Switch id="twoFactor" />
-								</div>
-							</div>
-							<Button className="mt-4">Update Account</Button>
-						</CardContent>
-					</Card>
-				</TabsContent>
-
-				{/* Appearance Tab */}
-				<TabsContent value="appearance">
-					<Card className="mt-4">
-						<CardHeader>
-							<h3 className="text-lg font-semibold">Appearance Settings</h3>
-						</CardHeader>
-						<CardContent>
-							<div className="space-y-4">
-								<div>
-									<Label htmlFor="theme">Theme</Label>
-									<div className="flex items-center space-x-4">
-										<Button variant="outline">Light</Button>
-										<Button variant="outline">Dark</Button>
-									</div>
-								</div>
-								<div>
-									<Label htmlFor="language">Language</Label>
-									<Input id="language" defaultValue="English" />
-								</div>
-							</div>
-							{canEdit ? (
-								<Button className="mt-4">Save Preferences</Button>
-							) : (
-								<Button className="mt-4">Edit</Button>
-							)}
+									<Field>
+										<FieldLabel htmlFor="username">Username</FieldLabel>
+										{canEdit ? (
+											<Input id="username" defaultValue={user.username} />
+										) : (
+											<p>{user.username} </p>
+										)}
+									</Field>
+									<Field>
+										<FieldLabel htmlFor="email">Email</FieldLabel>
+										{canEdit ? (
+											<p>
+												{user.email}{" "}
+												<i className="text-gray-500">
+													unavailable at this time
+												</i>
+											</p>
+										) : (
+											<p>{user.email}</p>
+										)}
+									</Field>
+									<Field>
+										<FieldLabel htmlFor="bio">Bio</FieldLabel>
+										{canEdit ? (
+											<Input id="bio" defaultValue={user.bio} />
+										) : (
+											<p>{user.bio}</p>
+										)}
+									</Field>
+									<Field>
+										{canEdit ? (
+											<>
+												<FieldLabel htmlFor="current-password">
+													Current Password
+												</FieldLabel>
+												<Input
+													id="current-password"
+													type="password"
+													placeholder="••••••••"
+												></Input>
+												<FieldLabel htmlFor="new-password">
+													New Password
+												</FieldLabel>
+												<Input
+													id="new-password"
+													type="password"
+													placeholder="••••••••"
+												></Input>
+												<FieldLabel htmlFor="confirm-new-password">
+													Confirm New Password
+												</FieldLabel>
+												<Input
+													id="new-password-confirm"
+													type="password"
+													placeholder="••••••••"
+												></Input>
+											</>
+										) : (
+											<>
+												<FieldLabel>Password</FieldLabel>
+												<p>••••••••</p>
+											</>
+										)}
+									</Field>
+								</FieldGroup>
+							</form>
 						</CardContent>
 					</Card>
 				</TabsContent>
